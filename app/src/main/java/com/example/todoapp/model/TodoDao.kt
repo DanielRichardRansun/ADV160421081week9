3ae5f7bc0cc9f0fd30d4989c0679d850e2428b39
native160421081 <richardgtwp@gmail.com>
2024-05-23 20:47:56 +0700

    Initial commit

---
+++ app/src/main/java/com/example/todoapp/model/TodoDao.kt
@@ -0,0 +1,30 @@
+package com.example.todoapp.model
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface TodoDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg todo:Todo)
+
+    @Query("SELECT * FROM todo WHERE is_done=0 ORDER BY priority DESC")
+    fun selectAllTodo(): List<Todo>
+
+    @Query("SELECT * FROM todo WHERE uuid= :id")
+    fun selectTodo(id:Int): Todo
+
+    @Delete
+    fun deleteTodo(todo:Todo)
+
+    @Query("UPDATE todo SET title=:title, notes=:notes, priority=:priority WHERE uuid = :id")
+    fun update(title:String, notes:String, priority:Int, id:Int)
+
+    // 0 = false, 1 = true
+    @Query("UPDATE todo SET is_done=1 WHERE uuid=:id")
+    fun doneTask(id:Int)
+
+}
